9c0c5a15565a59a96898490fe4193e339a678a7f
Abul Hassan Sheikh <asheikh@tdotperformance.ca>
2019-06-17 14:04:07 -0400

    Adding initial file June 17 2019

---
+++ PythonBasics.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Abul Hassan Sheikh
+#- git status: shows status of tracked and untracked files 
+#- git add [filename]: adds file to current commit 
+#- git commit -m '[message]': add commit locally 
+#- git push origin master: push commit to your github repository
+
+"""
+
+###Introduction of Python for Data Science
+##V1
+
+# Example, do not modify!
+print(5 / 8)
+
+# Put code below here
+print(7 + 10)
+
+# Division
+print(5 / 8)
+
+# Addition
+print(7 + 10)
+
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years?
+print(100*((1.1)**7))
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = savings * growth_multiplier ** 7
+
+# Print out result
+print(result)
+
+
+
+
+
