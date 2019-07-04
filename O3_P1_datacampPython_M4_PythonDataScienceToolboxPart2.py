1c900f690f43683770d625c344fe16ff344cf00e
Abul Hassan Sheikh <asheikh@tdotperformance.ca>
2019-07-04 15:13:53 -0400

    Syncing

--- O3_P1_datacampPython_M4_PythonDataScienceToolboxPart2.py
+++ O3_P1_datacampPython_M4_PythonDataScienceToolboxPart2.py
@@ -187,6 +187,23 @@ result_counts = count_entries('tweets.csv', 10, 'lang')
 print(result_counts)
 
 
+# Create a 5 x 5 matrix using a list of lists: matrix
+matrix = [[col for col in range(5)] for row in range(5)]
+
+# Print the matrix
+for row in matrix:
+    print(row)
+    
+    # Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create list comprehension: new_fellowship
+new_fellowship = [member for member in fellowship if len(member) >= 7]
+
+# Print the new list
+print(new_fellowship)
+
+
 
 
 
